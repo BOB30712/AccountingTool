20778127946f3818d626771e300972fb706e9662
Bobby <a0930023855@gmail.com>
2025-12-10 23:08:48 +0800

    test

---
+++ src/main/java/org/bob/App.java
@@ -0,0 +1,13 @@
+package org.bob;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
